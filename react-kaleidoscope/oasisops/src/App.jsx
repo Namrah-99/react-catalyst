ea919c15a8e5aade7bd1694d4c6bae2dc5740718
Namrah Saeed <namrahsaeed2@gmail.com>
2024-02-27 18:17:32 +0500

    feat(react-kaleidoscope): added react-router navigation and styled components for project (oasisops)

---
+++ react-kaleidoscope/oasisops/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />}></Route>
+            <Route path="dashboard" element={<Dashboard />}></Route>
+            <Route path="bookings" element={<Bookings />}></Route>
+            <Route path="cabins" element={<Cabins />}></Route>
+            <Route path="users" element={<Users />}></Route>
+            <Route path="settings" element={<Settings />}></Route>
+            <Route path="account" element={<Account />}></Route>
+          </Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ react-kaleidoscope/oasisops/src/ui/Heading.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+// const test = `text-align: center;
+// ${10 > 5 && "background-color: yellow"}`;
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    `font-size: 3rem;
+  font-weight: 600;
+ `}
+  ${(props) =>
+    props.as === "h2" &&
+    `font-size: 2rem;
+  font-weight: 600;
+  `}
+  ${(props) =>
+    props.as === "h3" &&
+    `font-size: 2rem;
+  font-weight: 500;
+  `}
+
+  line-height: 1.4
+`;
+
+export default Heading;
