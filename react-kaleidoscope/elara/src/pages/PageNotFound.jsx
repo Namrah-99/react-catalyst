8e718714687227397b07a335b4563775cb2e6761
Namrah Saeed <namrahsaeed2@gmail.com>
2024-03-27 00:38:52 +0500

    feat(react-kaleidoscope): restored categories file content (elara)

--- react-kaleidoscope/elara/src/pages/PageNotFound.jsx
+++ react-kaleidoscope/elara/src/pages/PageNotFound.jsx
@@ -1,18 +1,24 @@
 import { useMoveBack } from "../hooks/useMoveBack";
-import notfoundimg from "../assets/images/notfound3.mp4";
+import notfoundimg from "../assets/images/notfound5.mp4";
+import { BsBoxArrowLeft } from "react-icons/bs";
 
 function PageNotFound() {
   const moveBack = useMoveBack();
 
   return (
-    <div className="container mx-auto bg-[#f0f0f0 h-screen text-gray-900 grid place-content-center">
+    <div className="container mx-auto bg-[#f0f0f0 h-screen grid place-content-center">
       <div className="flex text-center justify-center items-center gap-12">
-        <div className="space-y-4">
-          <p className="text-h-lg font-semibold tracking-wider text-balance">
+        <div className="space-y-8">
+          <p className="text-gray-500 text-h-lg tracking-wider text-balance">
             The page you are looking for could not be found
           </p>
-          <button onClick={moveBack} size="large" className="uppercase">
-            &larr; Go back
+          <button
+            onClick={moveBack}
+            size="large"
+            className="text-gray-700 uppercase align-middle border border-gray-500 p-4"
+          >
+            <BsBoxArrowLeft className="inline-block mr-4 size-7" />
+            <b className="text-gray-600">Go back</b>
           </button>
         </div>
         <div>
@@ -27,10 +33,3 @@ function PageNotFound() {
 }
 
 export default PageNotFound;
-{
-  /* <video autoPlay loop muted>
-<source src="/path/to/your-animation.webm" type="video/webm" />
-<source src="/path/to/your-animation.mp4" type="video/mp4" />
-Your browser does not support the video tag.
-</video> */
-}
