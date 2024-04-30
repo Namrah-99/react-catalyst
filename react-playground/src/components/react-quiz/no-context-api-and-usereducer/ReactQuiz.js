4f1b282c4c3e52377349453a35139b092506ae65
Namrah Saeed <namrahsaeed2@gmail.com>
2024-04-30 13:23:16 +0500

    fix(react-playground): made changes to package.json and ReactQuiz.js files

--- react-playground/src/components/react-quiz/no-context-api-and-usereducer/ReactQuiz.js
+++ react-playground/src/components/react-quiz/no-context-api-and-usereducer/ReactQuiz.js
@@ -81,7 +81,7 @@ export default function ReactQuiz() {
   );
 
   useEffect(() => {
-    fetch("http://localhost:9000/questions")
+    fetch("/data/questions.json")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
